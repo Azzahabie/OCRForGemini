63fc023ac285e919a2100849969163b29df60424
azzahabie <azzahabie@gmail.com>
2021-11-20 16:09:41 +0800

    added more sample. basic calulations for profit and profitmargin available

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import cv2
 import pytesseract
+from re import sub
+from decimal import Decimal
 
 # list if transaction after being filtered
 filteredList = []
@@ -8,7 +10,7 @@ key_value_dict = {
 
 }
 # stores key_value_dict into a buy / sell dict
-sortbycoin_dict = {
+sortby_type_dict = {
     'buy': {
 
     },
@@ -37,13 +39,13 @@ def insert_into_dict(listItem):
             'coin': current_item[2],
             'buy/sell': current_item[3],
             'pricePerCoin': current_item[5],
-            'price': current_item[7],
-            'cost': current_item[8],
+            'coins': current_item[7],
+            'cost': current_item[9],
         }
         i += 1
 
 # takes dict and sorts it into buy / sell dict
-def sort_by_coin(list):
+def sort_by_type(list):
     buycount = 0
     sellcount = 0
     for item in list:
@@ -51,21 +53,46 @@ def sort_by_coin(list):
             if(value == 'coin'):
                 if(list[item]['buy/sell'] == 'Buy'):
                     buycount += 1
-                    buykey = sortbycoin_dict['buy']
+                    buykey = sortby_type_dict['buy']
                     buykey[buycount] = list[item]
                 else:
                     sellcount += 1
-                    sellkey = sortbycoin_dict['sell']
+                    sellkey = sortby_type_dict['sell']
                     sellkey[sellcount] = list[item]
 
+def calculate_total_by_type(dict, type):
+    tempTotal = 0
+    if(type == 'buy'):
+        for item in dict:
+            for index,value in enumerate(dict[item]):
+                tempindex = dict[item]
+                if(value == 'cost'):
+                    converted_cost = Decimal(sub(r'[^\d.]', '', tempindex[value]))
+                    tempTotal += converted_cost
+        return tempTotal
 
+    if(type == 'sell'):
+        for item in dict:
+            for index,value in enumerate(dict[item]):
+                tempindex = dict[item]
+                if(value == 'cost'):
+                    converted_cost = Decimal(sub(r'[^\d.]', '', tempindex[value]))
+                    tempTotal += converted_cost
+        return tempTotal
+
+def getImage():
+    print("Available Sample: 2lines, 3lines, 4lines, wide2, wide3, wide4, bigsample")
+    userinput = input("Pick Sample: ")
+    link = f"./sampleImages/{userinput}.png"
+    print(link)
+    return link
 
 
 
 # setting configs
 pytesseract.pytesseract.cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-img = cv2.imread('helloWorld.png')
-img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+img = cv2.imread(getImage())
+img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
 
 img_to_string = pytesseract.image_to_string(img)
 string_split = img_to_string.splitlines()
@@ -73,8 +100,16 @@ string_split = img_to_string.splitlines()
 # functions calling my mom
 filter_string_to_list(string_split)
 insert_into_dict(filteredList)
-sort_by_coin(key_value_dict)
-print(sortbycoin_dict)
+sort_by_type(key_value_dict)
+cost = calculate_total_by_type(sortby_type_dict['buy'], 'buy')
+revenue = calculate_total_by_type(sortby_type_dict['sell'], 'sell')
+
+netprofit = revenue - cost
+netprofitmargin = netprofit / revenue
+
+print(netprofit)
+print(netprofitmargin)
+
 
 
 
